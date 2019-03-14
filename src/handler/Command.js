9c0b4d70ea3f1be0d387cb00b4a80ad5ea6b74ce
Swords <33922797+Swords123456@users.noreply.github.com>
2019-03-14 16:53:58 +1100

    Update Command.js

--- src/handler/Command.js
+++ src/handler/Command.js
@@ -1,21 +1,27 @@
 let Toggable = require("./Toggable.js");
 class Command extends Toggable {
   constructor(options) {
+    super();
     this.name = options.name;
 
     // TODO: set all other properties
   }
   
   enable() {
+    //Calls the enable method in the Toggable;
+    super.enable();
     //logs maybe?
   }
   
   disable() {
+    //Calls the disable method in the Toggable;
+    super.disable();
     //logs maybe?
   }
 
   run() {
-    if (this.enabledd) {
+    //Checks if this command is enabled
+    if (this.enabled) {
       throw new Error(`Command '${this.name}' is missing run method`);
     }
   }
