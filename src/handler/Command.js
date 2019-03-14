051732b82b4c0a2324baa652a852a18aea31260c
Nicolas <36765044+waspothegreat@users.noreply.github.com>
2019-03-14 18:29:47 +0000

    Make Command class options optional (#24)

--- src/handler/Command.js
+++ src/handler/Command.js
@@ -3,9 +3,9 @@ class Command {
    * @description Create a new command
    * @param {string} name The name of the command
    * @param {Object} options The options for this command
-   * @param {Array<string>} options.aliases Aliases of this command
-   * @param {string} options.info Information about this command
-   * @param {string} options.usage Usage of this command
+   * @param {Array<string>} [options.aliases] Aliases of this command
+   * @param {string} [options.info] Information about this command
+   * @param {string} [options.usage] Usage of this command
    */
   constructor(name, options) {
     this.name = name;
@@ -13,17 +13,17 @@ class Command {
     if (!Array.isArray(options.aliases)) {
       throw new TypeError('Aliases must be an array');
     }
-    this.aliases = options.aliases;
+    this.aliases = options.aliases || [];
 
     if (!(typeof options.info === 'string')) {
       throw new TypeError('Info must be a string');
     }
-    this.info = options.info;
+    this.info = options.info || 'None';
 
     if (!(typeof options.usage === 'string')) {
       throw new TypeError('Usage must be a string');
     }
-    this.usage = options.usage;
+    this.usage = options.usage || 'None';
   }
 
   /**
