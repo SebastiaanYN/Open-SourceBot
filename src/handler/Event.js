696e50118027b3b71bb474605ff6cb8fea060e60
Swords <33922797+Swords123456@users.noreply.github.com>
2019-03-14 16:55:54 +1100

    Comments

--- src/handler/Event.js
+++ src/handler/Event.js
@@ -1,11 +1,14 @@
 let Toggable = require("./Toggable.js");
 class Event extends Toggable {
   constructor(eventName) {
+    //Calls super for Toggable
     super()
     this.eventName = eventName;
   }
-
+  
   run() {
+    //TODO Either place the check in the handler or a seperate method.
+    //Checks if the method is runnable, the runs it.
     if (this.enabled) {
      throw new Error('Event is missing run method'); 
     }
