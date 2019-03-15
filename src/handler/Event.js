ea23e90d38cafe1abf39a31f91f34cdc56e43680
Swords <Ibrahimhizmul@gmail.com>
2019-03-16 08:05:13 +1100

    Removing a line

--- src/handler/Event.js
+++ src/handler/Event.js
@@ -5,7 +5,6 @@ class Event extends Toggleable {
    * @description Create a new event
    * @param {string} eventName The name of the event
    */
-
   constructor(eventName) {
     // Calls super for Toggleable, (Super needs to be called)
     super();
