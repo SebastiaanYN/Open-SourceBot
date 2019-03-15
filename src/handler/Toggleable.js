607c5ac3e6695181877074288da9b4d0d08282d5
Swords <Ibrahimhizmul@gmail.com>
2019-03-15 23:50:44 +1100

    Comments

--- src/handler/Toggleable.js
+++ src/handler/Toggleable.js
@@ -5,21 +5,21 @@ class Toggleable {
 
   /**
    * @description Toggle isEnabled
-  */
+   */
   toggle() {
     this.isEnabled = !this.isEnabled;
   }
 
   /**
    * @description Changes isEnabled to true.
-  */
+   */
   enable() {
     this.isEnabled = true;
   }
 
   /**
    * @description Changes isEnabled to false.
-  */
+   */
   disable() {
     this.isEnabled = false;
   }
