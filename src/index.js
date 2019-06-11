48c3e59e776d0e8b0684ba09b618a273b94824c2
Anish Shobith <anish.shobith19@gmail.com>
2019-06-11 20:08:11 +0530

    Add minimum node requirement version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// This will check if the node version your are running is the required
+// Node version, if it isn't it throw the following error to inform
+// you.
+
+if (Number(process.version.slice(1).split('.')[0]) < 10) throw new Error('Node 10.0.0 or higher is required. Update Node on your system.');
+
 const path = require('path');
 
 const { Client } = require('discord.js');
