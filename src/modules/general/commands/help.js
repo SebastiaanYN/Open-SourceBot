f50599220a4b1002161ec2fee617c7d5aad5f608
Yanluis Fermin <tfermin1827@hotmail.com>
2019-07-13 01:28:55 -0700

    Improved help command

--- src/modules/general/commands/help.js
+++ src/modules/general/commands/help.js
@@ -1,26 +1,81 @@
+const { RichEmbed } = require('discord.js');
 const { Command } = require('../../../handler');
+const Utils = require('../../../Utils.js');
 
 module.exports = class extends Command {
   constructor({ commandHandler }) {
     super('help', {
       aliases: ['h', 'commands', 'cmds'],
-      info: `
-        Show all the commands or info about a specific command
-        **[]** is optional
-        **<>** is required
-      `,
+      info: 'Show all the commands or info about a specific command.',
       usage: 'help [command]',
       guildOnly: false,
+      contributors: ['Jacxk'],
     });
 
     this.commandHandler = commandHandler;
   }
 
-  run(message) {
-    const commands = Array.from(this.commandHandler.commands)
-      .map(([name, command]) => `${name} - ${command.aliases.join(', ')}`)
-      .join('\n');
+  run(message, args) {
+    const prefix = this.commandHandler.prefix;
+    let description;
 
-    message.channel.send(commands);
+    if (args.length === 0) {
+      description = `
+        __Features:__
+        ${Utils.stringify(
+          Array.from(this.commandHandler.features).map(
+            ([name, feature]) =>
+              `**${name}** - ${Utils.stringify(feature.commands)}`,
+          ),
+          '\n',
+        )}
+        
+        __Commands:__
+        ${Utils.stringify(
+          Array.from(this.commandHandler.commands).map(
+            ([, command]) => `**${prefix}${command.usage}** - ${command.info}`,
+          ),
+          '\n',
+        )}
+      `;
+    } else {
+      let command = this.commandHandler.commands.get(args[0]);
+
+      if (!command) {
+        command = this.commandHandler.aliases.get(args[0]);
+      }
+
+      if (!command) {
+        const embed = new RichEmbed()
+          .setTitle('Something when wrong!')
+          .setDescription('Invalid command provided, please try again!');
+
+        message.channel.send(embed);
+        return;
+      }
+
+      description = `
+        **Name:** ${command.name}
+        **Usage:** ${prefix}${command.usage}
+        **Info:** ${command.info}
+        **Aliases:** ${Utils.stringify(command.aliases)}
+        **Guild Only:** ${Utils.stringify(command.guildOnly)}
+        **Enabled:** ${Utils.stringify(command.guildOnly)}
+        
+        **Contributors:** ${Utils.stringify(
+          command.contributors.map(
+            username => `[${username}](https://github.com/${username})`,
+          ),
+        )}
+      `;
+    }
+
+    const embed = new RichEmbed()
+      .setTitle('Need help? Here are you go!')
+      .setDescription(
+        `${description}\n(**[]** *is optional*, **<>** *is required*)`,
+      );
+
+    message.channel.send(embed);
   }
 };
