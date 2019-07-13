b3d13443c431b75232cf766ed13e085d55833895
Yanluis Fermin <tfermin1827@hotmail.com>
2019-07-13 01:26:33 -0700

    Fixed issue with commands without aliases

--- src/handler/Command.js
+++ src/handler/Command.js
@@ -16,17 +16,18 @@ class Command extends Toggleable {
 
     this.name = name;
 
-    if (!Array.isArray(options.aliases)) {
+    this.aliases = options.aliases || [];
+
+    if (!Array.isArray(this.aliases)) {
       throw new TypeError('Aliases must be an array');
     }
-    if (options.aliases) {
-      options.aliases.forEach(alias => {
+    if (this.aliases) {
+      this.aliases.forEach(alias => {
         if (typeof alias !== 'string') {
           throw new TypeError('Aliases array must contain strings only');
         }
       });
     }
-    this.aliases = options.aliases || [];
 
     if (!(typeof options.info === 'string')) {
       throw new TypeError('Info must be a string');
